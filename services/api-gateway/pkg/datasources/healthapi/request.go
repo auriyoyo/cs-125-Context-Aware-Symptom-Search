50463a6f69a9b66ee53cc4ce594f419813ff7ee4
Kyle Yin Xu <kyleyinxu@gmail.com>
2026-02-10 17:59:47 -0800

    Add documentation for request body to clinical tables

--- services/api-gateway/pkg/datasources/healthapi/request.go
+++ services/api-gateway/pkg/datasources/healthapi/request.go
@@ -3,13 +3,22 @@ package healthapi
 // QueryRequest models the JSON body sent to the Health API.
 // All fields except Terms are optional.
 type QueryRequest struct {
-	Terms   []string `json:"terms"`
-	MaxList int      `json:"maxList,omitempty"`
-	Count   int      `json:"count,omitempty"`
-	Offset  int      `json:"offset,omitempty"`
-	Q       string   `json:"q,omitempty"`
-	Df      string   `json:"df,omitempty"`
-	Sf      []string `json:"sf,omitempty"`
-	Cf      string   `json:"cui,omitempty"`
-	Ef      []string `json:"ef,omitempty"`
+
+	// REQUIRED:
+	Terms []string `json:"terms"` // search query
+	// OPTIONAL:
+	MaxList int `json:"maxList,omitempty"` // max number of results, not compatible with pagination
+
+	// Pagination specs
+	Count  int `json:"count,omitempty"`  // number of results to fetch
+	Offset int `json:"offset,omitempty"` // offset (page number)
+
+	// Additional query specs
+	Q  string   `json:"q,omitempty"`  // additional wildcard queries
+	Sf []string `json:"sf,omitempty"` // comma-separated list of field names to search through
+
+	// Result manipulation
+	Cf string   `json:"cui,omitempty"` // field name for key used in return key list
+	Df string   `json:"df,omitempty"`  // field names for values in return values list
+	Ef []string `json:"ef,omitempty"`  // additional info (not bound to string type) to be attached to results
 }
